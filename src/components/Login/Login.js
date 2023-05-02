c0c9aa66e30fece68ac02323f3635d19d0861b9c
e536339 <maurin.isler@sbb.ch>
2023-05-02 16:32:21 +0200

    v1

---
+++ src/components/Login/Login.js
@@ -0,0 +1,53 @@
+import './Login.test';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Form from 'react-bootstrap/Form';
+import { useFormik } from 'formik';
+
+function Login() {
+
+  const login = useFormik({
+    initialValues: {
+      email: '',
+      password: ''
+    },
+    onSubmit: values => {
+      alert(JSON.stringify(values));
+      fetch('http://localhost:4242/api/login', { method: 'POST', body: JSON.stringify({ email: values.email, password: values.password }) } )
+        .then(response => response.json())
+        .then(data => alert(data.token) ) ;
+    },
+  });
+
+  return (
+      <Form onSubmit={login.handleSubmit}>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control 
+            type="email" 
+            name="email" 
+            placeholder="Enter email"
+            onChange={login.handleChange}
+            value={login.values.email}
+          />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control 
+            type="password" 
+            name="password" 
+            placeholder="Password"
+            onChange={login.handleChange}
+            value={login.values.password}
+          />
+        </Form.Group>
+
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+  );
+}
+
+export default Login;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Login from './components/Login/Login';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Login />
   </React.StrictMode>
 );
 
